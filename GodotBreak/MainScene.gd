a7a1abe7f98829be959edc9a0c290d27aa57973f
vivisuke <ntsuda@master.email.ne.jp>
2021-05-01 07:19:39 +0900

    0007 ◎ C 05/01 05/01 0.001│ToDo│Esc キー押下：ポーズ切り替え

--- GodotBreak/MainScene.gd
+++ GodotBreak/MainScene.gd
@@ -9,7 +9,7 @@ var vel = Vector2(200, -200)	# 右上方向
 func _ready():
 	pass
 func _input(event):
-	if event is InputEventKey && event.is_pressed() && event.unicode == 0x1b:	# ESC
+	if event is InputEventKey && event.pressed && event.scancode == KEY_ESCAPE:
 		pause = !pause
 	pass
 func _physics_process(delta):
@@ -20,5 +20,6 @@ func _physics_process(delta):
 		return
 	var collide = $Ball.move_and_collide(vel*delta)
 	if collide != null:
+		print(collide.collider.name)
 		vel = vel.bounce(collide.normal)
 	pass
