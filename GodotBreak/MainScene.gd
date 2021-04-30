11c675738c9380c9023fee41582dfd056678ad5b
vivisuke <ntsuda@master.email.ne.jp>
2021-05-01 06:24:04 +0900

    0006 ◎ C 05/01 05/01 0.001│ToDo│左右矢印キーでパドル左右移動

--- GodotBreak/MainScene.gd
+++ GodotBreak/MainScene.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
-var pause : bool = true
+const PAD_MOVE_UNIT = 400
+
+var pause : bool = false
 var vel = Vector2(200, -200)	# 右上方向
 
 
 func _ready():
 	pass
 func _input(event):
-	if event is InputEventKey and event.is_pressed():
+	if event is InputEventKey && event.is_pressed() && event.unicode == 0x1b:	# ESC
 		pause = !pause
 	pass
 func _physics_process(delta):
+	var dx = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	if dx != 0:
+		$Pad.move_and_collide(Vector2(dx*PAD_MOVE_UNIT, 0) * delta)
 	if pause:
 		return
 	var collide = $Ball.move_and_collide(vel*delta)
