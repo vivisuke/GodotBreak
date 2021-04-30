4ead793a38fe8b599c3dbf6a094e7be3ae7645e8
vivisuke <ntsuda@master.email.ne.jp>
2021-05-01 06:04:12 +0900

    0004 ◎ C 05/01 05/01 0.001│ToDo│キー押下：ポーズ切り替え

--- GodotBreak/MainScene.gd
+++ GodotBreak/MainScene.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
-var vel = Vector2(100, -100)	# 右上方向
+var pause : bool = true
+var vel = Vector2(200, -200)	# 右上方向
 
 
 func _ready():
 	pass
-
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		pause = !pause
+	pass
 func _physics_process(delta):
+	if pause:
+		return
 	var collide = $Ball.move_and_collide(vel*delta)
 	if collide != null:
 		vel = vel.bounce(collide.normal)
