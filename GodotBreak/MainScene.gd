d3ba8615358535460f1d1ce57189cf5c19475d60
vivisuke <ntsuda@master.email.ne.jp>
2021-05-01 13:31:51 +0900

    0017 ◎ C 05/01 05/01 0.001│ToDo│ブロック消去時：1/3 程度の確率で、（とりあえず）？アイテム落下

--- GodotBreak/MainScene.gd
+++ GodotBreak/MainScene.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 const SCREEN_WIDTH = 500
 const SCREEN_HEIGHT = 800
+const FRAME_WIDTH = 22
+const BLOCK_WIDTH = 38
+const BLOCK_HEIGHT = 16
+const BLOCK_Y0 = 100
 const PAD_MOVE_UNIT = 400
 const BALL_SPEED = 300
 
@@ -10,9 +14,22 @@ var started  : bool = false
 var vel = Vector2(BALL_SPEED, -BALL_SPEED)	# 右上方向
 var btQueue = []
 
+var rng = RandomNumberGenerator.new()
+var BlockYellow = load("res://BlockYellow.tscn")
 var BallTail = load("res://BallTail.tscn")
+var Question = load("res://Question.tscn")
 
+func setup_blocks():
+	for y in range(6):
+		var py = y * BLOCK_HEIGHT + BLOCK_Y0
+		for x in range(12):
+			var px = x * BLOCK_WIDTH + FRAME_WIDTH
+			var blk = BlockYellow.instance()
+			blk.position = Vector2(px, py)
+			add_child(blk)
 func _ready():
+	randomize()
+	setup_blocks()
 	pass
 func _input(event):
 	if event is InputEventKey && event.pressed:
@@ -23,8 +40,11 @@ func _input(event):
 func _physics_process(delta):
 	var dx = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	if dx != 0:
-		$Pad.move_and_collide(Vector2(dx*PAD_MOVE_UNIT, 0) * delta)
-	if !started:
+		var obj = $Pad.move_and_collide(Vector2(dx*PAD_MOVE_UNIT, 0) * delta)
+		if obj != null:
+			print(obj)
+			pass
+	if !started:	# ボールが飛翔中でない場合
 		$Ball.position.x = $Pad.position.x
 	if $Ball.position.y >= SCREEN_HEIGHT:
 		pause = true
@@ -36,8 +56,14 @@ func _physics_process(delta):
 		return
 	var collide = $Ball.move_and_collide(vel*delta)
 	if collide != null:
-		print(collide.collider.name)
+		#print(collide.collider.name)
 		vel = vel.bounce(collide.normal)
+		if collide.collider.name.left(6) == "@Block":	# ブロックを壊した場合
+			collide.collider.queue_free()
+			if rng.randi_range(0, 2) == 0:
+				var q = Question.instance()
+				q.position = $Ball.position
+				add_child(q)
 	pass
 func _on_BallTimer_timeout():
 	if !pause:
