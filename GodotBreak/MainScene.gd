0087e03627ac18ac73bf637edc79fda592be7eb8
vivisuke <ntsuda@master.email.ne.jp>
2021-05-05 12:52:15 +0900

    0036 ◎ C 05/02 05/05 0.001│ToDo│Pad の Area2D を削除

--- GodotBreak/MainScene.gd
+++ GodotBreak/MainScene.gd
@@ -13,6 +13,13 @@ const PAD_RADIUS = 12
 const PAD_CIRCLE_RADIUS = 48
 const BALL_SPEED = 300
 
+enum {
+	PAD_NORMAL = 0,
+	PAD_ROT90,
+	PAD_CIRCLE,
+	#PAD_ROTAITING,
+	N_PAD}
+
 var pause : bool = true
 var started  : bool = false
 var rot = 0.0
@@ -57,8 +64,34 @@ func is_collide_with_pad(lst):
 		if body.name.find("Pad") >= 0:
 			return true
 	return false
+func change_pad():
+	var r = rng.randi_range(0, N_PAD - 1)
+	print(r)
+	if r == PAD_NORMAL && (pad != $Pad || $Pad.rotation_degrees != 0):
+		if pad != $Pad:
+			$Pad.position = pad.position
+			pad.position = Vector2(-100, -100)
+			pad = $Pad
+		$Pad.rotation_degrees = 0
+		padRadius = PAD_CIRCLE_RADIUS
+		return
+	if r == PAD_ROT90 && (pad != $Pad || $Pad.rotation_degrees != 90):
+		if pad != $Pad:
+			$Pad.position = pad.position
+			pad.position = Vector2(-100, -100)
+			pad = $Pad
+		$Pad.rotation_degrees = 90
+		padRadius = PAD_CIRCLE_RADIUS
+		return
+	if r == PAD_CIRCLE && pad != $PadCircle:
+		$PadCircle.position = pad.position
+		pad.position = Vector2(-100, -100)
+		pad = $PadCircle
+		padRadius = PAD_CIRCLE_RADIUS
+		return
+	
 func _physics_process(delta):
-	$Pad.rotation += 5*delta
+	#$Pad.rotation += 5*delta
 	var dx = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	if dx != 0:
 		var obj = pad.move_and_collide(Vector2(dx*PAD_MOVE_UNIT, 0) * delta)
@@ -83,10 +116,13 @@ func _physics_process(delta):
 				fiQueue[ix] = null
 			else:
 				var lst = fiQueue[ix].get_colliding_bodies()
+				#print(lst.size())
 				if !lst.empty():
 					if is_collide_with_pad(lst):
+						#print("collide_with_pad")
 						fiQueue[ix].queue_free()
 						fiQueue[ix] = null
+						change_pad()
 	while !fiQueue.empty() && fiQueue[0] == null:
 		fiQueue.pop_front()
 	vel.y += 0.1		# 水平移動を始めた場合への対処
@@ -102,7 +138,7 @@ func _physics_process(delta):
 				add_child(q)
 				fiQueue.push_back(q)
 			nBlocks -= 1
-			print("nBlocks = ", nBlocks)
+			#print("nBlocks = ", nBlocks)
 			if nBlocks == 0:
 				init()
 	pass
@@ -125,17 +161,19 @@ func remove_question(body):
 			fiQueue[ix] = null
 			return
 func _on_Pad_body_entered(body):
-	print(body.name)
-	if body.name.find("Question") >= 0:
-		remove_question(body)
-		$PadCircle.position = $Pad.position
-		$Pad.position = Vector2(-100, -100)
-		pad = $PadCircle
-		padRadius = PAD_CIRCLE_RADIUS
+	return
+	#print(body.name)
+	#if body.name.find("Question") >= 0:
+	#	remove_question(body)
+	#	$PadCircle.position = $Pad.position
+	#	$Pad.position = Vector2(-100, -100)
+	#	pad = $PadCircle
+	#	padRadius = PAD_CIRCLE_RADIUS
 func _on_PadCircle_body_entered(body):
-	if body.name.find("Question") >= 0:
-		remove_question(body)
-		$Pad.position = $PadCircle.position
-		$PadCircle.position = Vector2(-100, -100)
-		pad = $Pad
-		padRadius = PAD_RADIUS
+	return
+	#if body.name.find("Question") >= 0:
+	#	remove_question(body)
+	#	$Pad.position = $PadCircle.position
+	#	$PadCircle.position = Vector2(-100, -100)
+	#	pad = $Pad
+	#	padRadius = PAD_RADIUS
